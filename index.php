8651ad8199d266f919190106a1a39f3f1c14637b
LiamOsler <liam.osler@dal.ca>
2021-11-14 22:40:55 -0400

    style new file modal

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
                 <i class="bi bi-geo"></i>
             </div>
             <br>
-            <button type="button" class="btn btn-secondary-outline btn-lg btn-block" data-toggle="modal" data-target="#exampleModal">
+            <button type="button" class="btn btn-outline-primary btn-lg btn-block" data-toggle="modal" data-target="#exampleModal">
                 New Survey
             </button>
-            <button type="button" class="btn btn-secondary btn-lg btn-block" data-toggle="modal" data-target="#exampleModal2">
+            <button type="button" class="btn btn-outline-primary  btn-lg btn-block" data-toggle="modal" data-target="#exampleModal2">
                 Load Survey
             </button>
             <button type="button" class="btn btn-primary btn-lg btn-block">Resume Latest Survey</button>
@@ -30,29 +30,71 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">New Survey</h5>
+        <h5 class="modal-title" id="exampleModalLabel">New Tree Survey</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-      <form class="form-inline">
+        <div class="input-group mb-3">
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="inputGroupFile02">
+                <label class="custom-file-label" for="inputGroupFile02">File Path</label>
+            </div>
+        </div>
+
+
+        <form class="form-inline">
         <div class="form-group">
-            <label for="inputPassword6">Password</label>
-            <input type="password" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
-            <small id="passwordHelpInline" class="text-muted">
-            Must be 8-20 characters long.
-            </small>
+            <label for="inputPassword6">File Name</label>
+            <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    .csv
+                </label>
+                </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                <label class="form-check-label" for="flexRadioDefault2">
+                    .xls
+                </label>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" checked>
+                <label class="form-check-label" for="flexRadioDefault3">
+                    .txt
+                </label>
+            </div>
+            
+            <br>
+            <div class="btn-group">
+                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    CRS (WGS84)
+                </button>
+                <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">WGS84</a>
+                <a class="dropdown-item" href="#">NAD83 CSRS</a>
+                <a class="dropdown-item" href="#">Clarke 1878</a>
+                </div>
+            </div>
+
+            <div class="btn-group">
+                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Datum (NAVD)
+                </button>
+                <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">CGVD</a>
+                <a class="dropdown-item" href="#">NAVD</a>
+                </div>
+            </div>
+            
         </div>
-    </form>  
+        </form>
 
 
-    <div class="input-group mb-3">
-        <div class="custom-file">
-            <input type="file" class="custom-file-input" id="inputGroupFile02">
-            <label class="custom-file-label" for="inputGroupFile02">File path</label>
-        </div>
-    </div>
+
 
 
       </div>
@@ -74,10 +116,14 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <div class="input-group mb-3">
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="inputGroupFile02">
+                <label class="custom-file-label" for="inputGroupFile02">File Path</label>
+            </div>
+        </div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
