b5c65e4b4bf7c638f7d6cb936ef558fe35d34636
LiamOsler <liam.osler@dal.ca>
2021-11-17 05:24:39 -0400

    new user landing modal

---
+++ includes/components/modals/newuser.php
@@ -0,0 +1,82 @@
+<div class="modal fade" id="newUserModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel"><i class="bi bi-tree"></i> Are You a New User?</h5>
+          <button type="button" class="close" aria-label="Close" data-dismiss="modal" data-backdrop="false" >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="images/video1.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="images/video2.png"  alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="images/video3.png"  alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <br>
+
+        <h6><i class="bi bi-person-circle"></i> Getting Started</h6>
+            <div class = "card file-box">
+                <i class="bi bi-person-plus"></i>
+                <p>Create Account</p>
+            </div>
+
+            <div class = "card file-box">
+                <i class="bi bi-journal"></i>
+                <p>Guides and Tutorials</p>
+            </div>
+
+            <div class = "card file-box">
+            <i class="bi bi-question-circle"></i>
+                <p>FAQ and Help</p>
+            </div>
+
+            <hr>
+            <h6><i class="bi bi-globe"></i> Arborist Community</h6>
+            <div class = "card file-box">
+            <i class="bi bi-chat-left-text"></i>
+                <p>Discussions</p>
+            </div>
+
+            <div class = "card file-box">
+                <i class="bi bi-newspaper"></i>
+                <p>News</p>
+            </div>
+
+            <div class = "card file-box">
+                <i class="bi bi-people"></i>
+                <p>People</p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal" data-backdrop="false" >Try Out Arborist</button>
+            </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
